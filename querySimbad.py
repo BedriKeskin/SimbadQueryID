d411d47799df4541bd38c2c7622d75ece9c92775
BedriKeskin <53687943+BedriKeskin@users.noreply.github.com>
2024-01-03 01:20:01 +0300

    Add files via upload

---
+++ querySimbad.py
@@ -0,0 +1,24 @@
+import numpy as np
+from astroquery.simbad import Simbad
+
+
+def SimbadQueryID(fromID, toID):
+    table = Simbad.query_objectids(fromID)
+    if table is not None:
+        string = table['ID'].astype(str)
+        table.replace_column('ID', string)
+        data = table['ID'].data
+        array = np.flatnonzero(np.core.defchararray.find(data, toID) != -1)
+        if len(array) == 1:
+            return data[array[0]].split()[-1]
+        else:
+            return None
+    else:
+        return None
+
+
+gaiaDR3ID = SimbadQueryID("kic7972785", 'DR3')
+if gaiaDR3ID is not None:
+    print(gaiaDR3ID)
+else:
+    print("bulunamadı")
